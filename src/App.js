73ce82354e1b81a4ba61fcb25785a037b2989b0e
SkrylDaria <43302081+SkrylDaria@users.noreply.github.com>
2023-02-05 20:47:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,19 +8,21 @@ import './styles/App.css'
 import Plays from './Plays';
 import Dream from './Dream';
 function App() {
-  const players = [{id:"Bit", url:"https://img-cdn.hltv.org/playerbodyshot/g-2t7HQ_d55I6VhRnP2URl.png?ixlib=java-2.1.0&w=400&s=90738194ed2b3fa7ded1c26ce0fcc20b", chosen:false, all:false},
-    {id:"Ax1le", url:"https://img-cdn.hltv.org/playerbodyshot/bm95B2mKVEWVyn0Ndk42pP.png?ixlib=java-2.1.0&w=400&s=a89edca0ff90d76a270097c032aaba4d", chosen:false, all:false},
-    {id:"Niko", url:"https://img-cdn.hltv.org/playerbodyshot/JDxiCoK-7_ZeBNCLNiSvFX.png?ixlib=java-2.1.0&w=400&s=d3ec14d8bad0ce37f4db73f1a82af92f", chosen:false, all:false},
-    {id:"Twistzz", url:"https://img-cdn.hltv.org/playerbodyshot/3W2WeF7zohMOjpjxGpurGG.png?ixlib=java-2.1.0&w=400&s=b81a58560cecf792aa16f0ee08f82ee4", chosen:false, all:false},
-    {id:"blameF", url:"https://img-cdn.hltv.org/playerbodyshot/2E6cPM2mB5MHoAXrP9fMlr.png?ixlib=java-2.1.0&w=400&s=e15f3d4349c719e0b63ce0d17bf1fe93", chosen:false, all:false}, {id:"electronic", url:"https://img-cdn.hltv.org/playerbodyshot/BugxKUSuXiyJVpK4P27Ys1.png?ixlib=java-2.1.0&w=400&s=df57dc790d42aa3c5f8d9c8cd9ffb1cc", chosen:false, all:false},
-    {id:"cadiaN", url:"https://img-cdn.hltv.org/playerbodyshot/VJ9qjM0bYy7MtycRp0tfdj.png?ixlib=java-2.1.0&w=400&s=bcc1e8b30a8f57880785180fc0f1b588", chosen:false, all:false},
-    {id:"Jame", url:"https://img-cdn.hltv.org/playerbodyshot/2Z5O3KO3RulVuGzSWUuk8d.png?ixlib=java-2.1.0&w=400&s=9b1bd9a92f6a947ffa877405fcb7a906", chosen:false, all:false},
-    {id:"karrigan", url:"https://img-cdn.hltv.org/playerbodyshot/_yByL-S5oSUgLpkQedassf.png?ixlib=java-2.1.0&w=400&s=932041b457de03d5c36a28f2557fbc24", chosen:false,all:false},
-    {id:"HooXi", url:"https://img-cdn.hltv.org/playerbodyshot/_-JN6JBvZdbSgwEzNcbolh.png?ixlib=java-2.1.0&w=400&s=df895b1929fac3c661a300284d18ed05", chosen:false, all:false}, {id:"s1mple", url:"https://img-cdn.hltv.org/playerbodyshot/Q2u6AgnDNYQ3dyObwN4JBX.png?ixlib=java-2.1.0&w=400&s=5e19fa63867872bd78409f6e757ff6c3", chosen:false, all:false},
-    {id:"ZywOo", url:"https://img-cdn.hltv.org/playerbodyshot/cDLEVO33Lh8PtHQtUyF4Q9.png?ixlib=java-2.1.0&w=400&s=5ebc8a972b11d0fd81bfd922f4e0902c", chosen:false, all:false},
-    {id:"oSee", url:"https://img-cdn.hltv.org/playerbodyshot/Bq-o0nulU4G2xOGoLp5AfH.png?ixlib=java-2.1.0&w=400&s=e11427343924455fd4c5ce888f32f521", chosen:false, all:false},
-    {id:"degster", url:"https://img-cdn.hltv.org/playerbodyshot/W_-FTMifJHCWw0YSdg_imt.png?ixlib=java-2.1.0&w=400&s=ba205fcfc9e41b9226902f27a1ad675d", chosen:false, all:false},
-    {id:"w0nderful", url:"https://img-cdn.hltv.org/playerbodyshot/BFLCtmUpq1hFfuWxlIEkcp.png?ixlib=java-2.1.0&w=400&s=04bfa57a75808bada7e2da5e42587725", chosen:false, all:false}]
+  const players = [{id:"Bit", url:"https://img-cdn.hltv.org/playerbodyshot/g-2t7HQ_d55I6VhRnP2URl.png?ixlib=java-2.1.0&w=400&s=90738194ed2b3fa7ded1c26ce0fcc20b", chosen:false, all:false, rat2: "1.0", kpr:"0.66", heads: "62.6%"},
+    {id:"Ax1le", url:"https://img-cdn.hltv.org/playerbodyshot/bm95B2mKVEWVyn0Ndk42pP.png?ixlib=java-2.1.0&w=400&s=a89edca0ff90d76a270097c032aaba4d", chosen:false, all:false, rat2: "1.11", kpr:"0.68", heads: "47.4%"},
+    {id:"Niko", url:"https://img-cdn.hltv.org/playerbodyshot/JDxiCoK-7_ZeBNCLNiSvFX.png?ixlib=java-2.1.0&w=400&s=d3ec14d8bad0ce37f4db73f1a82af92f", chosen:false, all:false, rat2: "1.24", kpr:"0.75", heads: "52.1%"},
+    {id:"Twistzz", url:"https://img-cdn.hltv.org/playerbodyshot/3W2WeF7zohMOjpjxGpurGG.png?ixlib=java-2.1.0&w=400&s=b81a58560cecf792aa16f0ee08f82ee4", chosen:false, all:false, rat2: "1.16", kpr:"0.73", heads: "54.1%"},
+    {id:"blameF", url:"https://img-cdn.hltv.org/playerbodyshot/2E6cPM2mB5MHoAXrP9fMlr.png?ixlib=java-2.1.0&w=400&s=e15f3d4349c719e0b63ce0d17bf1fe93", chosen:false, all:false, rat2: "1.33", kpr:"0.87", heads: "39.3%"}, 
+    {id:"electronic", url:"https://img-cdn.hltv.org/playerbodyshot/BugxKUSuXiyJVpK4P27Ys1.png?ixlib=java-2.1.0&w=400&s=df57dc790d42aa3c5f8d9c8cd9ffb1cc", chosen:false, all:false, rat2: "1.03", kpr:"0.65", heads: "45.1%"},
+    {id:"cadiaN", url:"https://img-cdn.hltv.org/playerbodyshot/VJ9qjM0bYy7MtycRp0tfdj.png?ixlib=java-2.1.0&w=400&s=bcc1e8b30a8f57880785180fc0f1b588", chosen:false, all:false, rat2: "1.07", kpr:"0.65", heads: "28.2%"},
+    {id:"Jame", url:"https://img-cdn.hltv.org/playerbodyshot/2Z5O3KO3RulVuGzSWUuk8d.png?ixlib=java-2.1.0&w=400&s=9b1bd9a92f6a947ffa877405fcb7a906", chosen:false, all:false, rat2: "1.20", kpr:"0.72", heads: "23.3%"},
+    {id:"karrigan", url:"https://img-cdn.hltv.org/playerbodyshot/_yByL-S5oSUgLpkQedassf.png?ixlib=java-2.1.0&w=400&s=932041b457de03d5c36a28f2557fbc24", chosen:false,all:false, rat2: "0.92", kpr:"0.55", heads: "48.7%"},
+    {id:"HooXi", url:"https://img-cdn.hltv.org/playerbodyshot/_-JN6JBvZdbSgwEzNcbolh.png?ixlib=java-2.1.0&w=400&s=df895b1929fac3c661a300284d18ed05", chosen:false, all:false, rat2: "0.89", kpr:"0.52", heads: "48.4%"}, 
+    {id:"s1mple", url:"https://img-cdn.hltv.org/playerbodyshot/Q2u6AgnDNYQ3dyObwN4JBX.png?ixlib=java-2.1.0&w=400&s=5e19fa63867872bd78409f6e757ff6c3", chosen:false, all:false, rat2: "1.16", kpr:"0.76", heads: "41.0%"},
+    {id:"ZywOo", url:"https://img-cdn.hltv.org/playerbodyshot/cDLEVO33Lh8PtHQtUyF4Q9.png?ixlib=java-2.1.0&w=400&s=5ebc8a972b11d0fd81bfd922f4e0902c", chosen:false, all:false, rat2: "1.26", kpr:"0.79", heads: "43.5%"},
+    {id:"oSee", url:"https://img-cdn.hltv.org/playerbodyshot/Bq-o0nulU4G2xOGoLp5AfH.png?ixlib=java-2.1.0&w=400&s=e11427343924455fd4c5ce888f32f521", chosen:false, all:false, rat2: "0.98", kpr:"0.61", heads: "28.9%"},
+    {id:"degster", url:"https://img-cdn.hltv.org/playerbodyshot/W_-FTMifJHCWw0YSdg_imt.png?ixlib=java-2.1.0&w=400&s=ba205fcfc9e41b9226902f27a1ad675d", chosen:false, all:false, rat2: "1.12", kpr:"0.71", heads: "33.5%"},
+    {id:"w0nderful", url:"https://img-cdn.hltv.org/playerbodyshot/BFLCtmUpq1hFfuWxlIEkcp.png?ixlib=java-2.1.0&w=400&s=04bfa57a75808bada7e2da5e42587725", chosen:false, all:false, rat2: "1.11", kpr:"0.69", heads: "30%"}]
 
   /*const [post, setPost] = useState([
     {id:1, title:'JS', body: 'descrip'},
@@ -47,6 +49,9 @@ function App() {
 			url  ={rifl.url}
 			chosen ={rifl.chosen}
       all={rifl.all}
+      rat2={rifl.rat2}
+      kpr={rifl.kpr}
+      heads={rifl.heads}
       addToDream={addToDream}
       changeState={changeState}
 		/>;
@@ -87,6 +92,7 @@ function App() {
 
   return (
     <div className="App place">
+      <title>DreamTeam app</title>
       <h1 className='wid' style={{color: "red"}}>Собери свою команду мечты
         <h6 style={{color: "black"}}>Правила игры: команда состоит из пяти игроков. В идеале, конечно, 3 рифлера, снайпер и кэп, но тут можно пофантазировать и бахнуть симпла и кэдиана в одну команду</h6>
       </h1>
@@ -95,7 +101,7 @@ function App() {
       <div>{itemsR}</div>
       </div>
       <div>
-        <h1 className='wid'>Dreamteam</h1>
+        <h1 className='fixed-top'>Dreamteam</h1>
         <div>{itemsRifl}</div> 
       </div>      
     </div>
