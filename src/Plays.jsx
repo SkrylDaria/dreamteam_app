327edc7790c94e83e023b3215c0d39b3a867c4d3
SkrylDaria <43302081+SkrylDaria@users.noreply.github.com>
2023-02-05 20:49:17 +0300

    Update Plays.jsx

--- src/Plays.jsx
+++ src/Plays.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 //import Dream from './Dream';
 
+
 //import Dream from './Dream';
 //import Dream from './Dream';
 import './styles/App.css'
@@ -9,9 +10,16 @@ const Plays = (props) => {
 //const [message, setMessage] = useState('');
   return (
     <div className='players'>
-        <div className='riflers'><img src={props.url} alt="display" width="200px" height="216px" /> </div>
-        <p><strong> {props.id}</strong> </p>
-        <p> {(props.chosen || props.all) ?  <button disabled>Добавить</button> : <button onClick={() => [props.addToDream(props.id), props.changeState()]}>Добавить</button>}  </p>
+      <div class='blog'>
+        <img src={props.url} alt="display" width="200px" height="216px"/> 
+        <div class="text">
+            <p>Rating 2.0: {props.rat2}</p>
+            <p>KPR: {props.kpr}</p>
+            <p>Headshots: {props.heads}</p>
+        </div>
+      </div>
+      <div><strong> {props.id}</strong> </div>
+      <p> {(props.chosen || props.all) ?  <button disabled>Добавить</button> : <button onClick={() => [props.addToDream(props.id), props.changeState()]}>Добавить</button>}  </p>
     </div>
   )
 };
